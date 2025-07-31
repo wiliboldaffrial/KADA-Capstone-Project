302c7bef1a4db5f4afbc6553a3cc49b30a518dac
Nadifah <nadifah.rahmani@student.president.ac.id>
2025-07-31 14:05:40 +0700

    update db patient management

--- backend/models/Patient.js
+++ backend/models/Patient.js
@@ -1,12 +1,16 @@
+Patient.js
+
 const mongoose = require('mongoose');
 
 const patientSchema = new mongoose.Schema({
-    name: String,
-    gender: String,
-    dateOfBirth: Date,
-    phone: String,
-    address: String,
-    checkups: [{type: mongoose.Schema.Types.ObjectId, ref: 'Checkup'}],
-})
+  nik: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  gender: String,
+  birthdate: Date,
+  bloodType: String,
+  contact: String,
+  address: String,
+  medicalHistory: String,
+}, { timestamps: true }); // `timestamps` adds createdAt and updatedAt fields
 
 module.exports = mongoose.model('Patient', patientSchema);
