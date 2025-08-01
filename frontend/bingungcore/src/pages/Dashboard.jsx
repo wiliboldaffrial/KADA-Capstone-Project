7d0fabb7da98c45d9ff1806575b518041578b7b5
qemal <gembol1419@gmail.com>
2025-08-01 15:20:27 +0700

    fix typo dashboard

--- frontend/bingungcore/src/pages/Dashboard.jsx
+++ frontend/bingungcore/src/pages/Dashboard.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import React, { useState, useEffect } from "react";
 import SideBar from "../components/SideBar";
 import PatientChartDay from "../components/PatientChartDay";
 import PatientBarChart from "../components/PatientBarMonth";
